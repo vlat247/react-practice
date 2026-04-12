faab68b1ab74f7c812a6ea9e090ca8b98d64a834
vlat <anonim270208@gmail.com>
2026-04-12 20:32:50 +0500

    refactor: replace App component with Counter and update project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
+  const [value, setValue] = useState('ALLAUHAKBAR')
+  const [isBombed, setIsBombed] = useState(false);
+
+  const handleBomb = () => {
+    setIsBombed(true);
+    alert("IRAN HAS BEEN BOMBED");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isBombed ? 'bombed' : ''}`}>
+      <h1>AMOUNT OF BOMBS THAT WILL BE THROWN ON IRAN:</h1>  
+      <Counter />
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}></input>
+      
+      <button id='btn' onClick={handleBomb}>BOMB IRAN</button>
     </div>
   );
 }
