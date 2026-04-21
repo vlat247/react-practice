6a2384391afe64b9940da407988386a52560241a
vlat <anonim270208@gmail.com>
2026-04-21 23:37:34 +0500

    feat: add post component structure and styling to the feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,21 @@
+
 export default function Feed() {
     return (
         <div className="container">
             <h1 style={{ marginTop: '50px' }}>This is the FEED page</h1>
             <p>The routing is working! You are now on the /feed route.</p>
+            <div className="feed-container">
+                <div className="post">
+                    <div className="post-content">
+                        <p>Java Script</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+
+                    </div>
+                    <div className="post-btn">
+                        <button>Delete</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
